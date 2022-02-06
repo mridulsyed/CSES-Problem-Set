1093dd0fada5e03c32eb2641805702caab3d71a7
Mridul Syed <mridulsyed11@gmail.com>
2022-02-06 22:13:52 +0600

    Add files via upload

---
+++ csset.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    int n; cin>>n;
+    int ans=0;
+    for(int i=5; i<=n; i*=5)
+    {
+        ans+=n/i;
+    }
+
+    cout<<ans;
+}
+
