ffb92f777bb97bfa984581d56f7fc5b2a8012b8f
Mridul Syed <mridulsyed11@gmail.com>
2022-02-06 11:15:20 +0600

    Add files via upload

---
+++ csset.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ll n;
+    cin >> n;
+    ll sum=0;
+
+    for(int i=1; i<n; ++i)
+    {
+        ll x; cin >> x;
+        sum+=x;
+    }
+
+    cout << n*(n+1)/2-sum;
+}
+
